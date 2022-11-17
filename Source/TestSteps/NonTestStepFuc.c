fa4cf993e2c8cd02dc04f2cb171d7f7a532a6dab
Matthew Li <lirujing0905@hotmail.com>
2022-11-17 16:20:08 -0500

    Implement ParseLogs

--- Source/TestSteps/NonTestStepFuc.c
+++ Source/TestSteps/NonTestStepFuc.c
@@ -14,6 +14,20 @@ int ParseLogs (char *LogPath, LogStruct *ParsedResults)
 	TSInit;
 	FILE* fp = fopen (LogPath, "r");
 	tsErrChk(!fp,"File not open");
+	char buff[1024];
+	
+	while (buff)
+	{
+		memset(buff,0,1024);
+		fgets(buff,1024,fp);
+		strtok(buff,"\n");
+		strcpy(ParsedResults->TestName, strtok(buff, ","));
+		ParsedResults->TestNum = atoi(strtok(NULL, ","));
+		strcpy(ParsedResults->TestVal,strtok(NULL, ","));
+		strcpy(ParsedResults->TestLoLim,strtok(NULL,","));
+		strcpy(ParsedResults->TestHiLim,strtok(NULL,","));
+		ParsedResults++;
+	}
 	
 Error:
 	return error;
